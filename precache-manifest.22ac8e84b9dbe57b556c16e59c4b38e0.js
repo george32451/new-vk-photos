474726c9c2f7512c5039236c827998894c923e07
gmarkosov <george32451@mail.ru>
2018-11-02 17:52:42 +0300

    Updates

---
+++ precache-manifest.22ac8e84b9dbe57b556c16e59c4b38e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "37a1a4635b4825f02c58",
+    "url": "/new-vk-photos/static/js/runtime~main.37a1a463.js"
+  },
+  {
+    "revision": "4f06f3342973c2c733be",
+    "url": "/new-vk-photos/static/js/main.4f06f334.chunk.js"
+  },
+  {
+    "revision": "fd2af1112173d0f0dd0c",
+    "url": "/new-vk-photos/static/js/1.fd2af111.chunk.js"
+  },
+  {
+    "revision": "4f06f3342973c2c733be",
+    "url": "/new-vk-photos/static/css/main.f012a6b5.chunk.css"
+  },
+  {
+    "revision": "fd2af1112173d0f0dd0c",
+    "url": "/new-vk-photos/static/css/1.b4e2c2ea.chunk.css"
+  },
+  {
+    "revision": "946bfbdaa18234ae7fe4e9993259cd7b",
+    "url": "/new-vk-photos/index.html"
+  }
+];
